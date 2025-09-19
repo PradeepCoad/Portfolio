6815444c85e7d47d38fa4720b327c04e2f386b53
Pradeep <pradeepverma156p@gmail.com>
2025-09-19 19:12:29 +0530

    contact page updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { Mail, Phone, MapPin, Send } from 'lucide-react';
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -17,12 +21,39 @@ const Contact = () => {
     }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAleZBBUbRcuxR5snNFgUHJQI1loYCE2eE",
+    authDomain: "portfolio-cfa12.firebaseapp.com",
+    projectId: "portfolio-cfa12",
+    storageBucket: "portfolio-cfa12.firebasestorage.app",
+    messagingSenderId: "38153401981",
+    appId: "1:38153401981:web:e5f5496a46ed771ad82314"
+  };
+
+  // Initialize Firebase
+  const fbApp = initializeApp(firebaseConfig);
+  const db = getFirestore(fbApp);
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     // Handle form submission here
     console.log('Form submitted:', formData);
-    // Reset form
-    setFormData({ name: '', email: '', subject: '', message: '' });
+
+    try {
+      await addDoc(collection(db, "formSubmissions"), formData);
+      console.log("Data saved to Firebase");
+
+      // Reset form
+      setFormData({ name: "", email: "", subject: "", message: "" });
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
   };
 
   const contactInfo = [
